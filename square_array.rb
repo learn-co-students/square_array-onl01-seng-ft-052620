540a2ff89340b1b23da1258712d48d2235234ce4
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 08:46:04 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each{|num| new_array<< num**2}
+  new_array
 end
+
+def square_array_collect(array)
+  array.collect{|num| num**2}
+end
+puts square_array_collect([1,3,6])
