0378e07f3e4f506c66ea2371b0f5d1b01e1e39df
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 13:56:04 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |item|
+    new_array.push(item ** 2)
+  end
+  new_array
 end
+
+=begin
+def square_array(array)
+  array.collect do |i|
+    i * i
+  end 
+end 
+=end
