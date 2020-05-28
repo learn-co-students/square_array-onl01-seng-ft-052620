9d1a2c072ccfa58b4f65a0106ee651a2341acdac
Grace Kim <goooyoon@gmail.com>
2020-05-28 01:32:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+end
+
+def square_array(array)
+array.each do |numbers|
+end
+end
+
+def square_array(array)
+squared = []
+array.each {|numbers| squared << numbers * numbers}
+squared
 end
