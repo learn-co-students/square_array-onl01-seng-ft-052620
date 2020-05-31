f42a53111141192a151eb18e3b44ac4931eaebb9
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-05-31 11:42:03 +0000

    Done.

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,22 +1,27 @@
-describe '#square_array' do
+ def square_array
+ square_array.each do |numbers|
+ end
+end 
 
-  it 'does not call on collect/map or inject' do
-    numbers = [1,2,3]
-    expect(numbers).to_not receive(:collect)
-    expect(numbers).to_not receive(:map)
-    expect(numbers).to_not receive(:inject)
-    square_array(numbers)
-  end
+# describe '#square_array' do
 
-  it 'calls on each' do
-    numbers = [1,2,3]
-    expect(numbers).to receive(:each)
-    square_array(numbers)
-  end
+#   it 'does not call on collect/map or inject' do
+#     numbers = [1,2,3]
+#     expect(numbers).to_not receive(:collect)
+#     expect(numbers).to_not receive(:map)
+#     expect(numbers).to_not receive(:inject)
+#     square_array(numbers)
+#   end
 
-  it 'should square the elements in an array' do
-    expect(square_array([1,2,3])).to eq([1,4,9])
-    expect(square_array([9,10,16,25])).to eq([81,100,256,625])
-  end
+#   it 'calls on each' do
+#     numbers = [1,2,3]
+#     expect(numbers).to receive(:each)
+#     square_array(numbers)
+#   end
 
-end
+#   it 'should square the elements in an array' do
+#     expect(square_array([1,2,3])).to eq([1,4,9])
+#     expect(square_array([9,10,16,25])).to eq([81,100,256,625])
+#   end
+
+# end
